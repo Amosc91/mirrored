c1e351ee347d4d037abbadd0f3e6b5af434a9279
amos <amosc91@gmail.com>
2021-12-27 15:12:27 +0200

    exercise6-fix

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,22 @@
-#!/bin/bash
 #add fix to exercise6-fix here
+#bash.sh
+array=("$@")
+dest=${array[-1]}
+array_len=$((${#array[@]}-2))
+sum=0
+FILESIZE=0
+ssh server2 [[ -d ${dest} ]];
+if [ $? -eq 1 ]; then
+        ssh server2 mkdir -p ${dest};
+        echo ${dest} does not exist in remote. creating folder...;
+
+fi
+
+for i in $(seq 0 $array_len);
+        do
+        scp ${array[$i]} vagrant@server2:$dest;
+        FILESIZE=$(stat -c%s ${array[$i]});
+        sum=$(( $sum + $FILESIZE));
+done
+
+echo  $sum;
