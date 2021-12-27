716dc85b9188c90a8e1dc3d6444129dcb38b3e72
Amos <amosc91@gmail.com>
2021-12-27 02:50:25 +0200

    exercise5-fix

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
-sudo sed -i '$ a 192.168.100.11 server2 server2' /etc/hosts
-mkdir -p /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
-chown -R vagrant:vagrant /home/vagrant/.ssh
+sudo -- sh -c -e "echo '192.168.100.11 server2' >> /etc/hosts";
 sudo su vagrant -c "ssh-keygen -t rsa -P ' ' -f /home/vagrant/.ssh/id_rsa"
+echo 'Host *
+  StrictHostKeyChecking no
+  UserKnownHostsFile /dev/null' > /home/vagrant/.ssh/config
 mkdir -p /vagrant/nfs/ssh
 cp /home/vagrant/.ssh/id_rsa.pub /vagrant/nfs/ssh/server2.pub
-ssh-keyscan -t rsa server2 > /etc/ssh/ssh_known_hosts
-ssh-keyscan -t dsa server2 >> /etc/ssh/ssh_known_hosts
-sleep 20
+ssh-keyscan -t rsa server2 >> /etc/ssh/ssh_known_hosts
 ssh-keyscan -t rsa server1 >> /etc/ssh/ssh_known_hosts
-ssh-keyscan -t dsa server1 >> /etc/ssh/ssh_known_hosts
 cat /vagrant/nfs/ssh/server2.pub >> /home/vagrant/.ssh/authorized_keys
 cat /vagrant/nfs/ssh/server1.pub >> /home/vagrant/.ssh/authorized_keys
